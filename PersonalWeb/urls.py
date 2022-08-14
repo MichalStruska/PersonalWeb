7dac39eb0eaa5806431cc68caf909516e97e500e
MichalStruska <struska1408@gmail.com>
2022-08-15 01:29:55 +0200

    navbar and stuff

--- PersonalWeb/urls.py
+++ PersonalWeb/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('yessir1408/', admin.site.urls),
     path('', include('blog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
