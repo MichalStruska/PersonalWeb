db044409bf5ddcab3f4f0bd9e67a52c8c8533d22
MichalStruska <struska1408@gmail.com>
2022-08-17 17:11:18 +0200

    make it better

--- PersonalWeb/settings.py
+++ PersonalWeb/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATIC_PATH02 = os.path.join(BASE_DIR, 'blog', 'static')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
@@ -139,6 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     STATIC_PATH,
+    STATIC_PATH02,
 )
 
 # Default primary key field type
