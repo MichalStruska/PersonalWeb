08074b9e59f4afd23e9fe06a556e2acfff3491a4
MichalStruska <struska1408@gmail.com>
2022-08-23 22:36:56 +0200

    a

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
 from django_extensions.db.fields import AutoSlugField
 from django.urls import reverse
 
