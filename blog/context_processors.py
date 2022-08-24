c8d677a86601137348369d56f40db3118a0325e0
MichalStruska <struska1408@gmail.com>
2022-08-24 10:03:06 +0200

    y

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,15 +1,15 @@
-# from .models import ProfileSingle
+from .models import ProfileSingle
 
-# def basic_info_process(request):
-#     user_info = ProfileSingle.objects.get(user_name="Michal Struska")
+def basic_info_process(request):
+    user_info = ProfileSingle.objects.get(user_name="Michal Struska")
     
-#     context={
-#         'facebook_link':user_info.facebook_link,
-#         'twitter_link':user_info.twitter_link,
-#         'github_link':user_info.github_link,
-#         'instagram_link':user_info.instagram_link,
-#         'department_link':user_info.department_link,
-#         'laboratory_link':user_info.laboratory_link,  
-#     }
-#     return context
+    context={
+        'facebook_link':user_info.facebook_link,
+        'twitter_link':user_info.twitter_link,
+        'github_link':user_info.github_link,
+        'instagram_link':user_info.instagram_link,
+        'department_link':user_info.department_link,
+        'laboratory_link':user_info.laboratory_link,  
+    }
+    return context
 
