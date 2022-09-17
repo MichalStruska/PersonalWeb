235281a55629ea32a1f6a5975b9a980d7dd3e07f
Michal Struška <michalstruska@Michals-MacBook-Air.local>
2022-09-18 01:20:41 +0200

    diff image zooming

--- PersonalWeb/settings.py
+++ PersonalWeb/settings.py
@@ -28,7 +28,7 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["10.0.1.15",
                  "localhost",
--- blog/static/js/image_zoom.js
+++ blog/static/js/image_zoom.js
@@ -4,29 +4,43 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+var images_sizeable = document.getElementsByClassName('sizeable')
 
 const images = document.querySelectorAll('input[type="img"]')
 
-images.forEach(image => {
-  image.addEventListener('click', aye)})
+// images_sizeable.forEach(image => {
+//   image.addEventListener('click', aye)})
 
+// document.querySelectorAll('.sizeable').addEventListener('click', aye);
+document.querySelectorAll('.sizeable').forEach(image => {
+    image.addEventListener('click', aye)});
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   console.log("aye");
+//   modalImg.src = this.src;
+//   // modalImg.style.width = "40vw";
+//   captionText.innerHTML = this.alt;
 
-img.onclick = function(){
+images_sizeable.onclick = function(){
+  modal.style.display = "block";
+  console.log("aye0");
+  modalImg.src = this.src;
+  // modalImg.style.width = "40vw";
+  captionText.innerHTML = this.alt;
+}
+//   // const images = document.querySelectorAll('input[type="img"]')
+
+//   // images.forEach(image => {
+//   // image.addEventListener('click', aye)})
+// }
+
+function aye() {
+  console.log("aye");
   modal.style.display = "block";
   console.log("aye");
   modalImg.src = this.src;
   // modalImg.style.width = "40vw";
   captionText.innerHTML = this.alt;
-
-
-  // const images = document.querySelectorAll('input[type="img"]')
-
-  // images.forEach(image => {
-  // image.addEventListener('click', aye)})
-}
-
-function aye() {
-  console.log("aye");
 }
 
 // Get the <span> element that closes the modal
