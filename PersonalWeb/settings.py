34d04afc370451fe217767481652919e6cbaa2c4
Michal Struška <michalstruska@michals-macbook-air.home>
2022-09-28 21:44:30 +0200

    fixed env issue

--- PersonalWeb/settings.py
+++ PersonalWeb/settings.py
@@ -28,7 +28,7 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["10.0.1.15",
                  "localhost",
